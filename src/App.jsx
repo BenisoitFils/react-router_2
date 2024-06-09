5a75bb53b37b5306d7b0754c274d5a2e2c965af3
BenisoitFils <pauhercharval@gmail.com>
2024-06-09 16:55:08 -0400

    Act3

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,21 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Pokemones from './components/Pokemones';
 import PokemonDetail from './components/PokemonDetail';
-import { Home } from './components/Home';
+import Home from './components/Home';
 import Navbar from './components/Navbar';
 
 const App = () => {
   return (
-    <div>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/pokemones" element={<Pokemones />} />
-          <Route path="/pokemon/:name" element={<PokemonDetail />} />
-        </Routes>
-      </Router>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pokemones" element={<Pokemones />} />
+        <Route path="/pokemon/:name" element={<PokemonDetail />} />
+      </Routes>
+    </>
   );
 };
 
 export default App;
+
